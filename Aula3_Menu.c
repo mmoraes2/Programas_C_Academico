d0ed8311f6c779fe6854021e3ad587b641ecfccb
Matheus Moraes <matheusmmoraes@yahoo.com.br>
2018-02-26 21:24:23 -0300

    Create Aula3_Menu.c

---
+++ Aula3_Menu.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    
+    int id; 
+    
+    printf("\n***********\n");
+    printf("\nCalculadora\n");
+    printf("\n***********\n");
+    
+    printf("\n1 - Soma");
+    printf("\n2 - Subtrair");
+    printf("\n3 - Sair");
+    
+    printf("\n\n***********\n");
+    
+    printf("\n\n Escolha a opcao desejada: ");
+    scanf("%d", &id);
+    
+    if(id == 1){
+        printf("\nSoma!");
+    }
+    if(id == 2){
+        printf("\nSubtracao!");
+    }
+    else
+
+    
+    return 0;
+}
