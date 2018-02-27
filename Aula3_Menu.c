c35753eaea34bb2f7984c7e315e7542ea5a41370
Matheus Moraes <matheusmmoraes@yahoo.com.br>
2018-02-27 19:45:03 -0300

    Update Aula3_Menu.c

--- Aula3_Menu.c
+++ Aula3_Menu.c
@@ -11,15 +11,15 @@ int sub(int num1, int num2){
 int menu(){
     int id; 
     
-    printf("\n***********");
-    printf("\nCalculadora");
-    printf("\n***********");
+    printf("\n***************");
+    printf("\n* Calculadora *");
+    printf("\n***************");
     
     printf("\n1 - Soma");
     printf("\n2 - Subtrair");
     printf("\n3 - Sair");
     
-    printf("\n***********\n");
+    printf("\n***************\n");
     
     do{
         printf("\nEscolha a opcao desejada: ");
@@ -28,10 +28,10 @@ int menu(){
     while(id<=0 || id>2);
     
     if(id == 1){
-        printf("\nSoma!");
+        printf("\nSoma!\n\n");
     }
     if(id == 2){
-        printf("\nSubtracao!");
+        printf("\nSubtracao!\n\n");
     }
 
     return id;
@@ -39,13 +39,23 @@ int menu(){
 
 int main(){
     
-    int m;
+    int m, n1, n2;
     m = menu();
     
     if(m==0)
         return 0;
     else
-    
+    {
+        printf("Informe o 1 numero: ");
+        scanf("%d", &n1);
+        printf("Informe o 2 numero: ");
+        scanf("%d", &n2);
+        
+        if(m == 1)
+            printf("\nSoma = %d", soma(n1, n2));
+        else
+            printf("\nSubtracao = %d", sub(n1, n2));
+    }
         
     return 0;
 }
