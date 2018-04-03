45c6b64a9c327f9608ec680138771093e2c17a7c
Matheus Moraes <matheusmmoraes@yahoo.com.br>
2018-04-03 08:32:28 -0300

    Create Aula12_Binario_Referencia.c

---
+++ Aula12_Binario_Referencia.c
@@ -0,0 +1,32 @@
+// Faça uma Função por Referencia que converta um numero inteiro para binário //
+#include <stdio.h>
+
+void binario(int *n){
+  int i, k;
+  
+  for (i = 31; i >= 0; i--)
+  {
+    k = *n >> i;
+ 
+    if (k & 1)
+      printf("1");
+    else
+      printf("0");
+  }
+}
+
+
+int main()
+{
+  int n, c, k;
+ 
+  printf("\n Entre com o numero decimal: ");
+  scanf("%d", &n);
+  
+  printf("\n Decimal: %d em binario e: ", n);
+  binario(&n);
+  
+  printf("\n");
+ 
+  return 0;
+}
